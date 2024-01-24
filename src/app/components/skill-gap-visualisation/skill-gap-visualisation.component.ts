d49a1bd12c62f41391f10dc5eba1f9037ef2bd28
hamayoonk38 <hamayoonk38@gmail.com>
2024-01-24 23:34:26 +0000

    Added skill gap analysis with dynamic chips and accordion
    - Implemented mat-accordion to categorise skills
    - Added mat-chip components to represent individual skills
    - Integrated mat-progress-bar to show progress within each category
    - Styled enhancements for better visual distinction between elements

--- src/app/components/skill-gap-visualisation/skill-gap-visualisation.component.ts
+++ src/app/components/skill-gap-visualisation/skill-gap-visualisation.component.ts
@@ -1,17 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-skill-gap-visualisation',
   standalone: true,
-  imports: [CommonModule],
+  encapsulation: ViewEncapsulation.None,
+  imports: [
+    CommonModule,
+    MatExpansionModule,
+    MatProgressBarModule,
+    MatTableModule,
+    MatChipsModule,
+  ],
   templateUrl: './skill-gap-visualisation.component.html',
   styleUrl: './skill-gap-visualisation.component.css',
 })
 export class SkillGapVisualisationComponent implements OnInit {
   skillsGap: any;
   categories: string[];
+  skillsForSelectedRole: any;
+  categoryStats: any[] = [];
 
   constructor(private route: ActivatedRoute, private router: Router) {}
 
@@ -20,10 +33,33 @@ export class SkillGapVisualisationComponent implements OnInit {
       // Retrieve the state
       const state = window.history.state;
 
-      if (state.skillsGap) {
+      if (state.skillsGap && state.categories && state.skillsForSelectedRole) {
         this.skillsGap = state.skillsGap;
         this.categories = state.categories;
+        this.skillsForSelectedRole = state.skillsForSelectedRole;
+        this.generateCategoryStats();
       }
     });
+  }
+
+  generateCategoryStats() {
+    this.categoryStats = this.categories.map((category) => {
+      const totalSkills = this.skillsForSelectedRole[category].length;
+      const missingSkills = this.skillsGap[category] || [];
+      const knownSkills = totalSkills - missingSkills.length;
+      const progressPercentage = (knownSkills / totalSkills) * 100;
+
+      return {
+        categoryName: category,
+        totalSkills,
+        knownSkills,
+        progressPercentage,
+        missingSkills,
+      };
+    });
+  }
+
+  isSkillMissing(stat: any, skill: any): boolean {
+    return stat.missingSkills.includes(skill.skill.skillName);
   }
 }
