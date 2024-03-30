33560e80ef788b96fe7b5d582f3f78a34ab12e6e
hamayoonk38 <hamayoonk38@gmail.com>
2024-03-30 14:10:26 +0000

    Added a warm-um service to start up the backend on load

--- src/app/services/job-info-service/job-info.service.spec.ts
+++ src/app/services/job-info-service/job-info.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { JobInfoService } from './job-info.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('JobInfoService', () => {
   let service: JobInfoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [JobInfoService],
+    });
+    
     service = TestBed.inject(JobInfoService);
   });
 
